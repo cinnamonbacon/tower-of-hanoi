917004ad522fd84454bee8ea32dd9625b9a8c0ad
cinnamonbacon <simon.bakan@gmail.com>
2024-01-21 22:27:09 -0500

    Fixed some bugs and got rid of unnecessaryanimations

--- drawTowers.js
+++ drawTowers.js
@@ -11,11 +11,12 @@ let sleepTime = 100;
 const visual = document.getElementById("stacks");
 var FrameSpeed;
 
-
+// draws all of the towers
 async function makeHTML(){
     document.getElementById("main").innerHTML="";
     for(let i=0; i<stacks.length; i++){
         var canvas = document.createElement("canvas");
+        canvas.id = "stack "+i;
         canvas.width = 250;
         canvas.height = 20+(stacks[0].length+1) * 40;
         var ctx = canvas.getContext("2d");
@@ -39,7 +40,23 @@ async function makeHTML(){
 
 }
 
-function setup() {
+// re-draws the nth tower out of 3.
+function redrawTower(n){
+    var canvas = document.getElementById("stack "+(n));
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log("drawing towers");
+    for(let j=0; j<stacks[0].length; j++){
+        const block = stacks[n][j];
+        if(block>0){
+            ctx.fillRect((110-60*(block/max)),25+30*j,10+(120*(block/max)),20);
+        }
+    }
+
+    ctx.fillRect(5,25+30*stacks[0].length,220,5);
+}
+
+async function setup() {
     max = parseInt(document.getElementById("num").value);
     stacks = [];
     stacks[0]=[];
@@ -66,12 +83,12 @@ async function controlClick(s){
     }
     acting = true;
     if (selected ==-1){
-        pickup(s);
+        await pickup(s);
         acting = false;
         return;
     }
     if (selected == s){
-        place(s,s);
+        await place(s,s);
         acting = false;
         return;
     }
@@ -85,13 +102,13 @@ async function controlClick(s){
     }
 
     if(top==-1 || top>stacks[selected][0]){
-        place(selected, s);
+        await place(selected, s);
         acting = false;
         return;
     }
 
+    await place(selected, selected);
     acting = false;
-    place(selected, selected)
 }
 
 async function pickup(s){
@@ -100,46 +117,31 @@ async function pickup(s){
         if(stacks[s][n]<0){
             n++;
         }else{
-            stacks[s][n-1]=stacks[s][n];
+            stacks[s][0]=stacks[s][n];
             stacks[s][n]=-1;
-            n--;
-            makeHTML();
-            await sleep(FrameSpeed);
+            console.log("picking up "+s+","+n);
+            redrawTower(s);
+            break;
+            //await sleep(FrameSpeed);
         }
     }
-    if(n==0){
+    if(n!=stacks[0].length){
         selected = s;
     }
 }
 
 async function place(s1, s2){
-    if(s1==s2){
-        var n=1
-        while(n<stacks[0].length && stacks[s1][n]<0){
-            await sleep(FrameSpeed);
-            stacks[s1][n]=stacks[s1][n-1];
-            stacks[s1][n-1]=-1
-            n++;
-            makeHTML();
-        }
-    }else if(Math.abs(s1-s2)==2){
-        await sleep(2*FrameSpeed);
-        stacks[1][0]=stacks[s1][0];
-        stacks[s1][0]=-1
+    var n=1
+    while(n<stacks[0].length && stacks[s2][n]<0){
+        //await sleep(FrameSpeed);
+        n++;
         makeHTML();
-        await sleep(2*FrameSpeed);
-        stacks[s2][0]=stacks[1][0];
-        stacks[1][0]=-1
-        makeHTML();
-        place(s2,s2);
-    }else{
-        await sleep(2*FrameSpeed);
-        console.log(s2);
-        stacks[s2][0]=stacks[s1][0];
-        stacks[s1][0]=-1
-        makeHTML();
-        place(s2,s2);
     }
+    stacks[s2][n-1]=stacks[s1][0];
+    stacks[s1][0]=-1
+    await redrawTower(s1);
+    await redrawTower(s2);
+
     selected = -1;
 }
 
@@ -181,8 +183,8 @@ async function solveTower(n,f){
         await sleep (sleepTime);
         await solveTower(n-1,(x)=>f(swap2(x)));
 
-        sleepTime/=1.5;
-        FrameSpeed/=1.5;
+        sleepTime/=1.1;
+        FrameSpeed/=1.1;
         //await sleep (sleepTime);
     }
 }
